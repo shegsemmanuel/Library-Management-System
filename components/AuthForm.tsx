55c59466abf1ee7b18252a6c9e083b468275f379
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2025-01-20 23:47:07 +0100

    auth UI

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,6 +15,9 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import Link from "next/link";
+import path from "path";
+import { FIELD_NAMES } from "@/constants";
 
 interface Props<T extends FieldValues> {
   schema: ZodType<T>;
@@ -29,6 +32,7 @@ const AuthForm = <T extends FieldValues>({
   defaultValues, 
   onSubmit,
 }: Props<T>) => {
+  const isSignIn = type === "SIGN_IN";
     const form :  UseFormReturn<T> = useForm({
       resolver: zodResolver(schema),
       defaultValues: defaultValues as DefaultValues<T>,
@@ -38,27 +42,53 @@ const AuthForm = <T extends FieldValues>({
     const handleSubmit: SubmitHandler<T> = async (data) => {};
 
   return (
-    <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn? "Welcome back to BookSmart!" : "Create Your library account"}
+      </h1>
+        <p className="text-light-100">
+          {isSignIn 
+          ? 'Access the vast collection of resources, and stay updated'
+          : "Please complete all fields and upload a valid university ID to gain access to the library."}
+        </p>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6 w-full"
+          >
+            {Object.keys(defaultValues).map((field) => (
+              <FormField
+              key={field}
+              control={form.control}
+              name={field as path<T>} 
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">{FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+            ))}
+
+        
       <Button type="submit">Submit</Button>
     </form>
   </Form>
+
+      <p className="text-center text-base font-medium">
+        {isSignIn ? "New to BookSmart? " : "Already have an account? "}
+
+        <Link href={isSignIn ? "/sign-up" : "/sign-in"}
+          className="font-bold text-primary"
+          >
+           {isSignIn ? "Create an account" : "Sign in"}
+        </Link>
+      </p>  
+    </div>
   )
 };
 export default AuthForm;
