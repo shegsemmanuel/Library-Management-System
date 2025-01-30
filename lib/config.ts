b6bf8b752a45845619ddb74b38fcf34956e9212a
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2025-01-30 15:51:08 +0100

    auth 2

--- lib/config.ts
+++ lib/config.ts
@@ -6,6 +6,7 @@ const config = {
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
             privatekey: process.env.IMAGEKIT_PRIVATE_KEY!,
        }, 
+       databaseUrl: process.env.DATABASE_URL!,   
     },
 };
 
